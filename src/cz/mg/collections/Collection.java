5b9791e2c5cab95a39564a38170127d621f0878f
Mg <gekoncze@seznam.cz>
2022-06-20 16:06:00 +0200

    Added debugging method to collection.

--- src/cz/mg/collections/Collection.java
+++ src/cz/mg/collections/Collection.java
@@ -3,4 +3,22 @@ package cz.mg.collections;
 import cz.mg.annotations.classes.Storage;
 
 public @Storage abstract class Collection<T> implements ReadableCollection<T>, WriteableCollection<T> {
+    /**
+     * This method is only used for easier debugging in IDEs that do not support displaying
+     * content of iterables during debugging.
+     */
+    protected Object[] debug() {
+        int count = 0;
+        for (Object ignored : this) {
+            count++;
+        }
+
+        Object[] array = new Object[count];
+        int i = 0;
+        for (Object o : this) {
+            array[i] = o;
+        }
+
+        return array;
+    }
 }
