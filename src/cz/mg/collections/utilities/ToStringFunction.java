6af3394f00d969a65a6e3280d4684d80625ad948
Mg <gekoncze@seznam.cz>
2023-03-11 20:26:15 +0100

    added missing annotation

--- src/cz/mg/collections/utilities/ToStringFunction.java
+++ src/cz/mg/collections/utilities/ToStringFunction.java
@@ -4,5 +4,5 @@ import cz.mg.annotations.classes.Utility;
 import cz.mg.annotations.requirement.Mandatory;
 
 public @Utility interface ToStringFunction<T> {
-    String toString(@Mandatory T t);
+    @Mandatory String toString(@Mandatory T t);
 }
